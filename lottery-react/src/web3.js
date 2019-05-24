bf98ba4b4c1d2f4958acf3e673902ecb18b8918c
hasanaliqureshi <hasanali.zeds@gmail.com>
2019-05-24 10:16:55 +0500

    configure web3

--- lottery-react/src/web3.js
+++ lottery-react/src/web3.js
@@ -1,5 +1,11 @@
 import Web3 from 'web3';
+let web3;
+async function activateMetamask() {
+    web3 = new Web3(window.ethereum);
 
-const web3 = new Web3(window.web3.currentProvider);
+    await window.ethereum.enable();
+}
+
+activateMetamask();
 
 export default web3;
